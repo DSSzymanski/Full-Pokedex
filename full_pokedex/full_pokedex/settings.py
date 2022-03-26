772184c8085cfe13c3deac57c8e68f9fc0c55d90
dsszymanski <dan1993us@yahoo.com>
2022-03-26 03:22:13 -0400

    Created app and reference in settings.

--- full_pokedex/full_pokedex/settings.py
+++ full_pokedex/full_pokedex/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pokedex.apps.PokedexConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
