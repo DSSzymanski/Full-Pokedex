a6f7c6c681bc97d5999375b4f3e9a2bade848478
dsszymanski <dan1993us@yahoo.com>
2022-04-16 02:23:12 -0400

    Redesigned log in page to also include a sign up form. Added styling to page.

--- full_pokedex/frontend/src/pages/LoginPage.js
+++ full_pokedex/frontend/src/pages/LoginPage.js
@@ -1,15 +1,24 @@
-import React, { useContext } from 'react'
-import AuthContext from "../context/AuthContext"
+import React, { useContext } from 'react';
+import AuthContext from "../context/AuthContext";
+import './LoginPage.css';
 
 const LoginPage = () => {
-  let { loginUser, user } = useContext(AuthContext);
+  let { loginUser } = useContext(AuthContext);
   return (
-    <div className='LoginFormDiv'>
-        <form onSubmit={ loginUser }>
-            <input type="text" name="username" placeholder="Enter Username." />
-            <input type="password" name="password" placeholder="Enter Password." />
-            <input type="submit" />
-        </form>
+    <div className='formDiv'>
+      <form id="loginForm" onSubmit={ loginUser }>
+        <h3>Log In</h3>
+        <input type="text" className='loginInput' name="username" placeholder="Enter Username." />
+        <input type="password" className='loginInput' name="password" placeholder="Enter Password." />
+        <input type="submit" className="submit" />
+      </form>
+      <form id="signUpForm" >
+        <h3>Sign Up</h3>
+        <input type="email" className='signUpInput' name="email" placeholder="Enter Email." />
+        <input type="text" className='signUpInput' name="username" placeholder="Enter Username." />
+        <input type="password" className='signUpInput' name="password" placeholder="Enter Password." />
+        <input type="submit" className="submit"/>
+      </form>
     </div>
   )
 }
