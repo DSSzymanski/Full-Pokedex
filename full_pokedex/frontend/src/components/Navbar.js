f102204bddd97a99c200695be61e0e0b19870b2f
dsszymanski <dan1993us@yahoo.com>
2022-04-18 20:19:19 -0400

    Added minor styling to Navbar.

--- full_pokedex/frontend/src/components/Navbar.js
+++ full_pokedex/frontend/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 //import { Link } from 'react-router-dom';
+import './Navbar.css'
 import AuthContext from '../context/AuthContext';
 
 const Navbar = () => {
