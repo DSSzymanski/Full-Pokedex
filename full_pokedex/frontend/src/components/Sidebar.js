ee034e3d2d16e1c8259e1d79c9e25b3ab5a0f474
dsszymanski <dan1993us@yahoo.com>
2022-06-21 23:26:57 -0400

    Added basic changes for if the window is mobile screen sized.

--- full_pokedex/frontend/src/components/Sidebar.js
+++ full_pokedex/frontend/src/components/Sidebar.js
@@ -53,9 +53,9 @@ const Sidebar = () => {
         {
           user ?
           <div className="logoutDiv">
-            <h6 onClick={ logoutUser }>
+            <div onClick={ logoutUser }>
               Logout
-            </h6>
+            </div>
           </div>
           :
           <div className="logoutDiv">
@@ -85,174 +85,186 @@ const Sidebar = () => {
           <div>
             Caught
           </div>
-          <div className="filter-item">
-            {
-              filterData['caught'] === TRUE ?
-              <input type="radio" name="caught" id="caught-true" value="true" defaultChecked/> :
-              <input type="radio" name="caught" id="caught-true" value="true"/>
-            }
-            <label htmlFor="caught-true">Caught</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['caught'] === FALSE ?
-              <input type="radio" name="caught" id="caught-false" value="false" defaultChecked/> :
-              <input type="radio" name="caught" id="caught-false" value="false"/>
-            }
-            <label htmlFor="caught-false">Not Caught</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['caught'] === ANY ?
-              <input type="radio" name="caught" id="caught-any" value="any" defaultChecked/> :
-              <input type="radio" name="caught" id="caught-any" value="any"/>
-            }
-            <label htmlFor="caught-any">Any</label>
+          <div className="radio-button-container">
+            <div className="filter-item">
+              {
+                filterData['caught'] === TRUE ?
+                <input type="radio" name="caught" id="caught-true" value="true" defaultChecked/> :
+                <input type="radio" name="caught" id="caught-true" value="true"/>
+              }
+              <label htmlFor="caught-true">Caught</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['caught'] === FALSE ?
+                <input type="radio" name="caught" id="caught-false" value="false" defaultChecked/> :
+                <input type="radio" name="caught" id="caught-false" value="false"/>
+              }
+              <label htmlFor="caught-false">Not Caught</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['caught'] === ANY ?
+                <input type="radio" name="caught" id="caught-any" value="any" defaultChecked/> :
+                <input type="radio" name="caught" id="caught-any" value="any"/>
+              }
+              <label htmlFor="caught-any">Any</label>
+            </div>
           </div>
         </div>
         <div className="filter-container">
           <div>
             Shiny
           </div>
-          <div className="filter-item">
-            {
-              filterData['shiny'] === TRUE ?
-              <input type="radio" name="shiny" id="shiny-true" value="true" defaultChecked/> :
-              <input type="radio" name="shiny" id="shiny-true" value="true"/> 
-            }
-            <label htmlFor="shiny-true">Shiny Obtained</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['shiny'] === FALSE ?
-              <input type="radio" name="shiny" id="shiny-false" value="false" defaultChecked/> :
-              <input type="radio" name="shiny" id="shiny-false" value="false"/>
-            }
-            <label htmlFor="shiny-false">Shiny Not Obtained</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['shiny'] === ANY ?
-              <input type="radio" name="shiny" id="shiny-any" value="any" defaultChecked/> :
-              <input type="radio" name="shiny" id="shiny-any" value="any"/>
-            }
-            <label htmlFor="shiny-any">Any</label>
+          <div className="radio-button-container">
+            <div className="filter-item">
+              {
+                filterData['shiny'] === TRUE ?
+                <input type="radio" name="shiny" id="shiny-true" value="true" defaultChecked/> :
+                <input type="radio" name="shiny" id="shiny-true" value="true"/> 
+              }
+              <label htmlFor="shiny-true">Shiny Obtained</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['shiny'] === FALSE ?
+                <input type="radio" name="shiny" id="shiny-false" value="false" defaultChecked/> :
+                <input type="radio" name="shiny" id="shiny-false" value="false"/>
+              }
+              <label htmlFor="shiny-false">Shiny Not Obtained</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['shiny'] === ANY ?
+                <input type="radio" name="shiny" id="shiny-any" value="any" defaultChecked/> :
+                <input type="radio" name="shiny" id="shiny-any" value="any"/>
+              }
+              <label htmlFor="shiny-any">Any</label>
+            </div>
           </div>
         </div>
         <div className="filter-container">
           <div>
             Lucky
           </div>
-          <div className="filter-item">
-            {
-              filterData['lucky'] === TRUE ?
-              <input type="radio" name="lucky" id="lucky-true" value="true" defaultChecked/> :
-              <input type="radio" name="lucky" id="lucky-true" value="true"/>
-            }
-            <label htmlFor="lucky-true">Lucky Obtained</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['lucky'] === FALSE ?
-              <input type="radio" name="lucky" id="lucky-false" value="false" defaultChecked/> :
-              <input type="radio" name="lucky" id="lucky-false" value="false"/>
-            }
-            <label htmlFor="lucky-false">Lucky Not Obtained</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['lucky'] === ANY ?
-              <input type="radio" name="lucky" id="lucky-any" value="any" defaultChecked/> :
-              <input type="radio" name="lucky" id="lucky-any" value="any"/>
-            }
-            <label htmlFor="lucky-any">Any</label>
+          <div className="radio-button-container">
+            <div className="filter-item">
+              {
+                filterData['lucky'] === TRUE ?
+                <input type="radio" name="lucky" id="lucky-true" value="true" defaultChecked/> :
+                <input type="radio" name="lucky" id="lucky-true" value="true"/>
+              }
+              <label htmlFor="lucky-true">Lucky Obtained</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['lucky'] === FALSE ?
+                <input type="radio" name="lucky" id="lucky-false" value="false" defaultChecked/> :
+                <input type="radio" name="lucky" id="lucky-false" value="false"/>
+              }
+              <label htmlFor="lucky-false">Lucky Not Obtained</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['lucky'] === ANY ?
+                <input type="radio" name="lucky" id="lucky-any" value="any" defaultChecked/> :
+                <input type="radio" name="lucky" id="lucky-any" value="any"/>
+              }
+              <label htmlFor="lucky-any">Any</label>
+            </div>
           </div>
         </div>
         <div className="filter-container">
           <div>
             Shadow
           </div>
-          <div className="filter-item">
-            {
-              filterData['shadow'] === TRUE ?
-              <input type="radio" name="shadow" id="shadow-true" value="true" defaultChecked/> :
-              <input type="radio" name="shadow" id="shadow-true" value="true"/>
-            }
-            <label htmlFor="shadow-true">Shadow Obtained</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['shadow'] === FALSE ?
-              <input type="radio" name="shadow" id="shadow-false" value="false" defaultChecked/> :
-              <input type="radio" name="shadow" id="shadow-false" value="false"/>
-            }
-            <label htmlFor="shadow-false">Shadow Not Obtained</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['shadow'] === ANY ?
-              <input type="radio" name="shadow" id="shadow-any" value="any" defaultChecked/> :
-              <input type="radio" name="shadow" id="shadow-any" value="any"/>
-            }
-            <label htmlFor="shadow-any">Any</label>
+          <div className="radio-button-container">
+            <div className="filter-item">
+              {
+                filterData['shadow'] === TRUE ?
+                <input type="radio" name="shadow" id="shadow-true" value="true" defaultChecked/> :
+                <input type="radio" name="shadow" id="shadow-true" value="true"/>
+              }
+              <label htmlFor="shadow-true">Shadow Obtained</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['shadow'] === FALSE ?
+                <input type="radio" name="shadow" id="shadow-false" value="false" defaultChecked/> :
+                <input type="radio" name="shadow" id="shadow-false" value="false"/>
+              }
+              <label htmlFor="shadow-false">Shadow Not Obtained</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['shadow'] === ANY ?
+                <input type="radio" name="shadow" id="shadow-any" value="any" defaultChecked/> :
+                <input type="radio" name="shadow" id="shadow-any" value="any"/>
+              }
+              <label htmlFor="shadow-any">Any</label>
+            </div>
           </div>
         </div>
         <div className="filter-container">
           <div>
             Purified
           </div>
-          <div className="filter-item">
-            {
-              filterData['purified'] === TRUE ?
-              <input type="radio" name="purified" id="purified-true" value="true" defaultChecked/> :
-              <input type="radio" name="purified" id="purified-true" value="true"/>
-            }
-            <label htmlFor="purified-true">Purified Obtained</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['purified'] === FALSE ?
-              <input type="radio" name="purified" id="purified-false" value="false" defaultChecked/> :
-              <input type="radio" name="purified" id="purified-false" value="false"/>
-            }
-            <label htmlFor="purified-false">Purified Not Obtained</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['purified'] === ANY ?
-              <input type="radio" name="purified" id="purified-any" value="any" defaultChecked/> :
-              <input type="radio" name="purified" id="purified-any" value="any"/>
-            }
-            <label htmlFor="purified-any">Any</label>
+          <div className="radio-button-container">
+            <div className="filter-item">
+              {
+                filterData['purified'] === TRUE ?
+                <input type="radio" name="purified" id="purified-true" value="true" defaultChecked/> :
+                <input type="radio" name="purified" id="purified-true" value="true"/>
+              }
+              <label htmlFor="purified-true">Purified Obtained</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['purified'] === FALSE ?
+                <input type="radio" name="purified" id="purified-false" value="false" defaultChecked/> :
+                <input type="radio" name="purified" id="purified-false" value="false"/>
+              }
+              <label htmlFor="purified-false">Purified Not Obtained</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['purified'] === ANY ?
+                <input type="radio" name="purified" id="purified-any" value="any" defaultChecked/> :
+                <input type="radio" name="purified" id="purified-any" value="any"/>
+              }
+              <label htmlFor="purified-any">Any</label>
+            </div>
           </div>
         </div>
         <div className="filter-container">
           <div>
             Mega
           </div>
-          <div className="filter-item">
-            {
-              filterData['mega'] === TRUE ?
-              <input type="radio" name="mega" id="mega-true" value="true" defaultChecked/> :
-              <input type="radio" name="mega" id="mega-true" value="true"/>
-            }
-            <label htmlFor="mega-true">Mega Obtained</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['mega'] === FALSE ?
-              <input type="radio" name="mega" id="mega-false" value="false" defaultChecked/> :
-              <input type="radio" name="mega" id="mega-false" value="false"/>
-            }
-            <label htmlFor="mega-false">Mega Not Obtained</label>
-          </div>
-          <div className="filter-item">
-            {
-              filterData['mega'] === ANY ?
-              <input type="radio" name="mega" id="mega-any" value="any" defaultChecked/> :
-              <input type="radio" name="mega" id="mega-any" value="any"/>
-            }
-            <label htmlFor="mega-any">Any</label>
+          <div className="radio-button-container">
+            <div className="filter-item">
+              {
+                filterData['mega'] === TRUE ?
+                <input type="radio" name="mega" id="mega-true" value="true" defaultChecked/> :
+                <input type="radio" name="mega" id="mega-true" value="true"/>
+              }
+              <label htmlFor="mega-true">Mega Obtained</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['mega'] === FALSE ?
+                <input type="radio" name="mega" id="mega-false" value="false" defaultChecked/> :
+                <input type="radio" name="mega" id="mega-false" value="false"/>
+              }
+              <label htmlFor="mega-false">Mega Not Obtained</label>
+            </div>
+            <div className="filter-item">
+              {
+                filterData['mega'] === ANY ?
+                <input type="radio" name="mega" id="mega-any" value="any" defaultChecked/> :
+                <input type="radio" name="mega" id="mega-any" value="any"/>
+              }
+              <label htmlFor="mega-any">Any</label>
+            </div>
           </div>
         </div>
         <div className="btn-div">
